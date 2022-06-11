a6386deb3df5efcca0083bd92e268d57e95af5ca
Kruhlikau Aleh <akruhlikau@sncsoft.com>
2022-06-11 13:02:10 +0300

    api structure change

--- blog-backend/app/api/v1/urls.py
+++ blog-backend/app/api/v1/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from posts.views import PostView, DetailPost
+from users.views import UserListApiView, UserDetailApiView, CreateUserApiView, LoginUserAPIView
 
 urlpatterns = [
     path('<int:pk>/', DetailPost.as_view()),
     path('', PostView.as_view()),
+    path('users/login/', LoginUserAPIView.as_view(), name='login'),
+    path('users/', UserListApiView.as_view(), name='users_list'),
+    path('users/<int:pk>/', UserDetailApiView.as_view(), name='user_detail'),
+    path('users/create/', CreateUserApiView.as_view(), name='create'),
 ]
