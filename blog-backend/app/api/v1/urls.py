20fb3ecf6590870c5f9a63342f5d907dd2e24eee
Kruhlikau Aleh <akruhlikau@sncsoft.com>
2022-06-22 16:16:18 +0300

    url, api fixes

--- blog-backend/app/api/v1/urls.py
+++ blog-backend/app/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
 urlpatterns = [
-    # path('posts/<int:pk>/', DetailPost.as_view()),
-    # path('posts/', PostView.as_view()),
     path('posts/', include('posts.urls')),
+    path('users/', include('user.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
